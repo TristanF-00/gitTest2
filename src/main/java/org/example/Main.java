b8abcaab8693eb388360faf4cecd241308c6e511
TristanF-00 <tfsaypharath@gmail.com>
2025-02-18 20:45:48 -0500

    3rd commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("version 2");
+        System.out.println("version 3");
     }
 }
